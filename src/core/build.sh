01b00c0c7679478ca21ed709982e6b2c86546ffa
bereket-fiseha <bereketfiseha123@gmail.com>
2025-05-17 13:18:07 +0300

    modified for deployment

--- src/core/build.sh
+++ src/core/build.sh
@@ -2,7 +2,7 @@ set -o errexit
 
 pip install -r src/core/requirements.txt
 
-python manage.py collectstatic --no-input
+python src/core/manage.py collectstatic --no-input
 
 
-python manage.py migrate
+python src/core/manage.py migrate
